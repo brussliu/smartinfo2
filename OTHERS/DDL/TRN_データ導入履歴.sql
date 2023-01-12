794d30c2c174ed01d9dae649cd60c2bc24b391fe
brussliu <38742004+brussliu@users.noreply.github.com>
2023-01-12 21:32:07 +0900

    aa
    aa

--- OTHERS/DDL/TRN_データ導入履歴.sql
+++ OTHERS/DDL/TRN_データ導入履歴.sql
@@ -3,6 +3,7 @@ drop table public."TRN_データ導入履歴";
 create table public."TRN_データ導入履歴" (
   "店舗ID" character varying(20)
   , "データ種別" character varying(20)
+  , "導入ファイル名" character varying(200)
   , "導入日時" timestamp(6) without time zone
   , "導入件数" integer
 );
