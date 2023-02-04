349c36c488d33b15810af62a6175c7b1df7b5c62
lijinghui <08048982566jp@gmail.com>
2023-02-04 20:11:35 +0900

    02-04

--- OTHERS/DDL/RES_コスト情報.sql
+++ OTHERS/DDL/RES_コスト情報.sql
@@ -1,6 +1,6 @@
 drop table public."RES_コスト情報";
 
-create table public."RES_コスト情報" (
+create table public."HIS_コスト情報" (
     "登録日" timestamp(6) without time zone
   , "発生日" timestamp(6) without time zone
   , "ステータス" character varying(20)
@@ -10,7 +10,7 @@ create table public."RES_コスト情報" (
   , "金額（円）" numeric(10,2)
   , "為替レート" numeric(6,2)
   , "備考" character varying(200)
-　, "店舗ID" character varying(50)
+  , "店舗ID" character varying(50)
   , "登録日時" timestamp(6) without time zone
   , "更新日時" timestamp(6) without time zone
 );
