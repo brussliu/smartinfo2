21d4a4fb04a5c6441b0f76973ec8770722bc51a3
sl <763375553@qq.com>
2023-03-12 19:58:15 +0800

    纳品DDL

---
+++ OTHERS/DDL/TRN_納品明細.sql
@@ -0,0 +1,13 @@
+drop table public."TRN_”[•iŠÇ—";
+
+create table public."TRN_”[•iŠÇ—" (
+
+  , "”[•iNO" character varying(20)
+  , "SKU”Ô†" character varying(50)
+  , "ASIN”Ô†" character varying(50)
+  , "”—Ê" integer
+  , "ó—Ì”—Ê" integer
+  , "“X•ÜID" character varying(20)
+  , "“o˜^“ú" timestamp(6) without time zone
+  , "XV“ú" timestamp(6) without time zone
+);
