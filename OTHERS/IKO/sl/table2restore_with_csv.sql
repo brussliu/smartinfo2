6a3a77c469058da86f777bf525944b10131ffb36
sl <763375553@qq.com>
2023-03-18 15:18:20 +0800

    update

--- OTHERS/IKO/sl/table2restore_with_csv.sql
+++ OTHERS/IKO/sl/table2restore_with_csv.sql
@@ -1,3 +1,3 @@
-COPY "MST_ソート情報" from 'D:\\111.csv' with csv header;
-COPY "MST_出品マスタ情報" from 'D:\\222.csv' with csv header;
-COPY "MST_在庫情報" from 'D:\\333.csv' with csv header;
+COPY "MST_ソート情報" from 'D:\\100.csv' with csv header;
+COPY "MST_出品マスタ情報" from 'D:\\200.csv' with csv header;
+COPY "MST_在庫情報" from 'D:\\300.csv' with csv header;
