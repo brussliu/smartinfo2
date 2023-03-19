cc728f4cce89cb7e93ed1a424c573797a017882b
brussliu <38742004+brussliu@users.noreply.github.com>
2023-03-19 23:23:45 +0900

    ddd
    sss

--- OTHERS/IKO/sl/table2backup_to_csv.sql
+++ OTHERS/IKO/sl/table2backup_to_csv.sql
@@ -1,3 +1,5 @@
 COPY "MST_ソート情報" to 'D:\\100.csv' with csv header;
 COPY "MST_出品マスタ情報" to 'D:\\200.csv' with csv header;
 COPY "MST_在庫情報" to 'D:\\300.csv' with csv header;
+COPY "MST_入庫仕入推奨数量情報" to 'D:\\400.csv' with csv header;
+COPY "MST_ユーザー情報" to 'D:\\500.csv' with csv header;
--- OTHERS/IKO/sl/table2restore_with_csv.sql
+++ OTHERS/IKO/sl/table2restore_with_csv.sql
@@ -1,3 +1,5 @@
 COPY "MST_ソート情報" from 'D:\\100.csv' with csv header;
 COPY "MST_出品マスタ情報" from 'D:\\200.csv' with csv header;
 COPY "MST_在庫情報" from 'D:\\300.csv' with csv header;
+COPY "MST_入庫仕入推奨数量情報" from 'D:\\400.csv' with csv header;
+COPY "MST_ユーザー情報" from 'D:\\500.csv' with csv header;
